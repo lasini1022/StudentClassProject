0d31f5c73d9b3f88fba6aa5aa99caa35eefa4aa4
lasiniehansa <63825414+lasiniehansa@users.noreply.github.com>
2020-04-18 13:58:26 -0400

    update

--- StudentClassProject.java
+++ StudentClassProject.java
@@ -1,5 +1,4 @@
-java.io.File;
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
@@ -9,54 +8,46 @@ import java.util.StringTokenizer;
 
 public class StudentClassProject {
     private static final String filename = "C:\\users\\Owner\\Desktop\\StudentNames.txt";
-    static String [] students; //hold the list of students
+    static ArrayList<String> students; //hold the list of students
     private static Scanner in = new Scanner(System.in);
     static ArrayList<String> courses = null;
 
-    public static void main(String[] args) {
-        try {
-            File names_file = new File(filename); // Specify the filename
-            Scanner myReader = new Scanner(names_file);
-            
-            int line_num = 1;
-            //read the file
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine(); //read in one line from a while
-                System.out.print (line_num);
-                //tokenize the line
-                StringTokenizer st = new StringTokenizer(data, "|");
-                System.out.println (" Name: " + st.nextToken());
-                System.out.println ("Student #: " + st.nextToken());
-                System.out.println ("DOB: " + st.nextToken());
-                System.out.println ("Courses: " + st.nextToken());
-                line_num++;
-            }
-            myReader.close();
-
-        } catch (FileNotFoundException e) {
-            //file is not there, so create it
-
-            System.out.println("How many students?");
-            int num_names = in.nextInt();
-            students = new String[num_names];
-            for (int i = 0; i < num_names; i++) { //go through all students
-                students = getUserInput();
-            }
-            writeFile();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws IOException {
+        System.out.println("How many students?");
+        int num_names = in.nextInt();
+        //students = new String[num_names];//doesn't need since it is a array list
+        for (int i = 0; i < num_names; i++) { //go through all students
+            students.add(getUserInput());
         }
+        writeFile();
+        File names_file = new File(filename); // Specify the filename
+        Scanner myReader = new Scanner(names_file);
+
+        int line_num = 1;
+        //read the file
+        while (myReader.hasNextLine()) {
+        String data = myReader.nextLine(); //read in one line from a while
+        System.out.print (line_num);
+        //tokenize the line
+        StringTokenizer st = new StringTokenizer(data, "|");
+        System.out.println (" Name: " + st.nextToken());
+        System.out.println ("Student #: " + st.nextToken());
+        System.out.println ("DOB: " + st.nextToken());
+        System.out.println ("Courses: " + st.nextToken());
+        line_num++;
+        }
+        myReader.close();
 
     }
 
 
 
 
-    private static ArrayList<String> getUserInput() throws IOException {
+    private static String getUserInput() {
         System.out.println("Type student name and press enter");
         //String input = in.next() + "|";
         Student s = null;
+        
         //System.out.println("Type student number and press enter");
         String name = in.next();
         System.out.println("Type how may courses does a student have and press enter: ");
@@ -74,11 +65,9 @@ public class StudentClassProject {
         }while (month < 1 || month > 12);
 
         int day;
-        do {
-            System.out.println("Type day of birth");
-            day = in.nextInt();
-        }while(local(year,month,day));
-        assert s != null;
+        System.out.println("Type day of birth");
+        day = in.nextInt();
+        assert false;
         s.courseList();
         System.out.println("Type in course code, or type done if finished");
         for (int i = 0; i < n; i++) { //ask for 4 courses
@@ -87,23 +76,17 @@ public class StudentClassProject {
             courses.add(i,course);
             //input += course + ",";
         }
-        s = new Student (name, LocalDate.of(year,month,day),courses);
-
-        return "l";
+        LocalDate DOB = LocalDate.of(year,month,day);
+        Student a = new Student (name, DOB ,courses);
+        String nom = a.getName();
+        int id = a.getID();
+        StringBuilder co= null;
+        for (String cours : courses) {
+            co.append(cours).append(",");
+        }
+        return nom+"|"+id+"|"+DOB+"|"+co;//can later add student and room number
     }
-
-    /**
-     * Checks if the year, month, and day entered is valid.
-     * @param year year user enters for DOB
-     * @param month month user enters for DOB
-     * @param day day user enters for DOB
-     * @return false if the three variables were able to make a
-     * @throws IOException if date is not valid
-     */
-    private static boolean local (int year, int month, int day) throws IOException {
-        LocalDate date = LocalDate.of(year,month,day);
-        return false;
-    }
+    
     private static void writeFile() {
 
         try {
