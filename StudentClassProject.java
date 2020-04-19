0992f6004a9c4968d8b954a729c785791e33ea3d
Vhershey <63884364+Vhershey@users.noreply.github.com>
2020-04-19 18:23:53 -0400

    Update StudentClassProject.java

--- StudentClassProject.java
+++ StudentClassProject.java
@@ -1,9 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -18,14 +15,17 @@ public class StudentClassProject {
     private static final String filename = "C:\\users\\Owner\\Desktop\\StudentNames.txt";
     static ArrayList<String> students; //hold the list of students
     private static Scanner in = new Scanner(System.in);
-    static ArrayList<String> courses = null;
+    private static ArrayList<String> courses = new ArrayList<>();
     static final String file = "src/CourseList";
+    static final String teacher = "src/TeacherList";
+    private static ArrayList<String> educators = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
+        System.out.println("");
         System.out.println("How many students?");
-        int num_names = in.nextInt();
+        int num_students = in.nextInt();
         //students = new String[num_names];//doesn't need since it is a array list
-        for (int i = 0; i < num_names; i++) { //go through all students
+        for (int i = 0; i < num_students; i++) { //go through all students
             students.add(getUserInput());
         }
         writeFile();
@@ -36,16 +36,16 @@ public class StudentClassProject {
         //read the file
         while (myReader.hasNextLine()) {
             String data = myReader.nextLine(); //read in one line from a while
-            System.out.print (line_num);
+            System.out.print(line_num);
             //tokenize the line
             StringTokenizer st = new StringTokenizer(data, "|");
-            System.out.println (" Name: " + st.nextToken());
-            System.out.println ("Student #: " + st.nextToken());
-            System.out.println ("DOB: " + st.nextToken());
-            System.out.println ("Courses: " + st.nextToken());
+            System.out.println(" Name: " + st.nextToken());
+            System.out.println("Student #: " + st.nextToken());
+            System.out.println("DOB: " + st.nextToken());
+            System.out.println("Courses: " + st.nextToken());
             line_num++;
         }
-        myReader.close();
+        //myReader.close();
         //asking for deleting and adding new students
         System.out.println("Would you want to add or delete student, if not press done? \n Note:Type in only lowercase");
         System.out.print("type add/delete/done: ");
@@ -56,45 +56,75 @@ public class StudentClassProject {
             switch (extra) {
                 case "add":
                     getUserInput();
-                     break;
+                    break;
                 case "delete":
                     System.out.println("Please type in the name of the student that you will like to delete? ");
-                    String deleteStudent = in.nextLine();
-                    for(int i=0;i<students.size();i++) {
-                        String st = students.get(i);
-                        StringTokenizer tokenizer = new StringTokenizer(st, "|");
-                        String token = tokenizer.nextToken();
-                        if (deleteStudent.equals(token)) {
-                            students.remove(i);
-                        }
-                    }
-                    System.out.println("So this is the new list of students name");
-
-                    for(int j=0; j<students.size();j++){
-                        System.out.println(students.get(j));
-                    }
 
                     break;
                 case "done":
                     break label;
                 default:
-                    System.out.println("Sorry, you have not typed ");
+                    System.out.println("Sorry, you have not typed the proper ");
                     break;
             }
+        } while (extra.equals("add") || extra.equals("delete"));
+
+        System.out.println("Would would you want to add or delete a teacher, if not type delete");
+        System.out.println("delete/add/done");
+        String user = in.nextLine();
+
+        switch (user) {
+            case "done":
+                break;
+
+            case "delete":
+                System.out.println("Please type in the teacher would you like to delete ");
+                teacherInput();
+
+                break;
+            case "add":
+                getteacheradd();
+                break;
         }
-
-        while (extra.equals("add") || extra.equals("delete"));
-
     }
+
+    /**
+    * @return the list of teachers after deleting it
+    * @param teach
+    * @param course 
+    * @
+    * */
+    private static String teacherInput(String teach,String course) {
+        System.out.println("Type teacher name and press enter");
+        teach = in.nextLine();
+
+        System.out.println("Type in the course: ");
+        course = in.nextLine();
+
+            if(teach.equals(teacher)){
+                educators.remove(teach);
+            }
+        return teacher;
+    }
+
+    private static String getteacheradd(){
+        System.out.println("Type teacher name and press enter");
+        String teach = in.nextLine();
+
+        System.out.println("Type in the course: ");
+        String course = in.nextLine();
+
+        if(teach.equals(teacher)){
+            educators.add(teach);
+        }
+        return teacher;
+    }
+
+
 
     private static String getUserInput() {
         System.out.println("Type student name and press enter");
-        //String input = in.next() + "|";
-        //Student s = null;
-        //System.out.println("Type student number and press enter");
         String name = in.next();
-        System.out.println("Type how may courses does a student have and press enter: ");
-        int n = in.nextInt();
         int year;
         do {
             System.out.println("Type year of birth");
@@ -110,35 +140,44 @@ public class StudentClassProject {
         int day;
         System.out.println("Type day of birth");
         day = in.nextInt();
-        assert false;
-        courseList();
-        System.out.println("Type in course code, or type done if finished");
-        for (int i = 0; i < n; i++) { //ask for 4 courses
-            String course = in.nextLine();
-            Course c = new Course (n, course, courses);
-
-            // if (course.equals("done")) break;
-            if(c.addCourse()) {
-                courses.add(course);
+        System.out.println("Type how many courses does the student have then press enter: ");
+        int count_stop = in.nextInt();
+        System.out.println("These are the courses you can pick from:");
+        courseList_Print();
+        System.out.println("Type in course code and type done when finished: ");
+        String course;
+        // int counter =0;
+        Course c = new Course (count_stop);
+        for(int h=0; h<count_stop; h++) {
+            course = in.nextLine();
+            if(course.equals("done")){break;}
+            else if (c.courseList(course) && c.check_course_format(course)) {
+                if (c.addCourses(course).equals(course)) {
+                    courses.add(course);
+                    h++;
+                }
+            } else {
+                h--;
             }
-            //input += course + ",";
+        }
+        System.out.println(c.progress());
+        for(String part: courses){
+            System.out.println(part);
         }
         Student a = new Student (name, LocalDate.of(year,month,day));
         String nom = a.getName();
         LocalDate DOB = a.getDOB();
         int id = a.getID();
-        StringBuilder co= null;
-        for (String cours : courses) {
-            co.append(cours).append(",");
+        String co="";
+        for (int g=0; g<courses.size(); g++) {
+            co+=courses.get(g)+",";
         }
         return nom+"|"+id+"|"+DOB+"|"+co;//can later add student and room number
     }
 
     private static void writeFile() {
-
         try {
             FileWriter myWriter = new FileWriter(filename);
-
             for (String student : students) {
                 myWriter.write(student + "\r\n");
             }
@@ -147,10 +186,9 @@ public class StudentClassProject {
         catch(IOException error){
             System.out.println("error occurred creating a file");
         }
-
-
     }
-    public static void courseList(){
+
+    public static void courseList_Print(){
         String data;
         try {
             File myObj = new File(file);
@@ -165,6 +203,4 @@ public class StudentClassProject {
             e.printStackTrace();
         }
     }
-
 }
-
